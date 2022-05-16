85947ef122850ac77eca01dd200ae15bcc8e3cec
nishanbe <79426795+nishanbe@users.noreply.github.com>
2022-05-16 23:08:30 +1000

    Update header.php

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 		?>
 
 		<header id="site-header" class="header-footer-group">
-<h1>NISHAN Bedrossia</h1>
+<h1>NISHAN Bedrossian</h1>
 			<div class="header-inner section-inner">
 
 				<div class="header-titles-wrapper">
